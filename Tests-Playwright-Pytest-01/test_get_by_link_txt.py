11a76334cd11122b778135425bdbc8dc7bbcb05b
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-09-08 15:27:35 +0300

    Refactor code

--- Tests-Playwright-Pytest-01/test_get_by_link_txt.py
+++ Tests-Playwright-Pytest-01/test_get_by_link_txt.py
@@ -6,32 +6,24 @@ with sync_playwright() as playwright: # This Is For Starting sync playwright Usi
     # This Will Start Chromium Browser With Headless-Option AS False
     # And The Slow Mode Will Increase x1000
     browser = playwright.chromium.launch(headless=False, slow_mo=1000)
-
     # This Will Create New Page Using The Browser
     page = browser.new_page()
-
     # This Will Change The Browser URL
     page.goto("https://playwright.dev/python")
 
     # This Will Return The Links With Text=Docs
     docs_links = page.get_by_role('link', name='Docs')
-
     # This Will Highlight The Main Object
     docs_links.highlight()
-
     # The Page-Object.url Will Return The Current URL
     print("The Current URL Is: ", page.url)
-
     # This Will Fire The Click-Event
     docs_links.click()
-
     print("The Current URL Is: ", page.url)
 
     # This Will Close The Page
     page.close()
-
     # This Will Close The Browser
     browser.close()
-
     # Here AS We Use with-Keyword, Then stop()-Method Will Call Automatically
     # playwright.stop()
--- Tests-Playwright-Pytest-01/test_get_by_role.py
+++ Tests-Playwright-Pytest-01/test_get_by_role.py
@@ -6,36 +6,25 @@ import re
 
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False, slow_mo=1000)
-
     page = browser.new_page()
-
     page.goto("https://bootswatch.com/default/")
 
     btn = page.get_by_role('button', name='Default button')
-
     btn.scroll_into_view_if_needed()
-
     print("The Button Is: ", btn)
 
     heading = page.get_by_role('heading', name='Heading 2')
-
     heading.scroll_into_view_if_needed()
-
     print("The Heading Is: ", heading)
 
     radio_btn = page.get_by_role('radio', name=re.compile("^(Option One)", re.IGNORECASE))
-
     radio_btn.scroll_into_view_if_needed()
-
     print("The Radio BTN Is: ", radio_btn)
     print("The Radio BTN is_visible Is: ", radio_btn.is_visible())
 
     checkbox = page.get_by_role('checkbox', name=re.compile("Default Checkbox", re.IGNORECASE))
-
     checkbox.scroll_into_view_if_needed()
-
     # checkbox has method name: check()-method that check/uncheck the status of checkbox
-
     print("The Checkbox Is: ", checkbox)
     print("The Checkbox is_visible Is: ", checkbox.is_visible())
 
@@ -48,35 +37,24 @@ with sync_playwright() as playwright:
     # email_input.highlight()
 
     password_input = page.locator("#floatingPassword")
-
     password_input.scroll_into_view_if_needed()
-
     password_input.highlight()
 
     email_input_by_placeholder = page.get_by_placeholder("name@example.com")
-
     email_input_by_placeholder.scroll_into_view_if_needed()
-
     email_input_by_placeholder.highlight()
 
     text_element = page.get_by_text("with faded secondary text")
-
     text_element.scroll_into_view_if_needed()
-
     text_element.highlight()
 
     middle_element = page.get_by_text("Middle")
-
     middle_element.scroll_into_view_if_needed()
-
     middle_element.highlight()
 
     text_element = page.get_by_text("attr", exact=True)
-
     print("The element with exact is, display: ", text_element.is_visible())
-
     text_element.scroll_into_view_if_needed()
-
     text_element.highlight()
 
     # password_input_by_placeholder = page.get_by_placeholder("Password")
