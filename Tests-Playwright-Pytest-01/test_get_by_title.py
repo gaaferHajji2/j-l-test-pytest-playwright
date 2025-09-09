f314143045a4bc62ba9158a8ad37d30949067ebd
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-09-09 13:06:07 +0300

    test get by title-attr

---
+++ Tests-Playwright-Pytest-01/test_get_by_title.py
@@ -0,0 +1,13 @@
+from playwright.sync_api import sync_playwright
+import time
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False, slow_mo=1000)
+    page = browser.new_page()
+    page.goto("https://bootswatch.com/default/")
+    page.get_by_title("attribute").scroll_into_view_if_needed()
+    page.get_by_title("attribute").highlight()
+
+    time.sleep(2)
+    page.close()
+    browser.close()
