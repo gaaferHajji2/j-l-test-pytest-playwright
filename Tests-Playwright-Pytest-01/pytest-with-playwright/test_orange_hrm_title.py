79ccd5e8a6af10b9847d3187f22bf52dbc231441
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-11-11 15:06:35 +0300

    Build first test for orange hrm

---
+++ Tests-Playwright-Pytest-01/pytest-with-playwright/test_orange_hrm_title.py
@@ -0,0 +1,11 @@
+from playwright.sync_api import Page
+
+def test_orange_hrm_title(page: Page):
+    page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    username = page.get_by_placeholder("Username")
+    username.fill("Admin")
+    password = page.get_by_placeholder("Password")
+    password.fill("admin123")
+    page.get_by_role("button", name="Login").click()
+
+    assert page.url == "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
