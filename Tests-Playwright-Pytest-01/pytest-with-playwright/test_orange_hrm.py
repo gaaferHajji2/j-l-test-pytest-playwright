c435b22b1ed3810c120c7ab8f271cee69db3da76
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-11-13 12:50:46 +0300

    delete unused code

--- Tests-Playwright-Pytest-01/pytest-with-playwright/test_orange_hrm.py
+++ Tests-Playwright-Pytest-01/pytest-with-playwright/test_orange_hrm.py
@@ -1,5 +1,4 @@
 from playwright.sync_api import Page
-import time
 
 def test_orange_hrm_title(page: Page):
     page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
@@ -8,5 +7,4 @@ def test_orange_hrm_title(page: Page):
     password = page.get_by_placeholder("Password")
     password.fill("admin123")
     page.get_by_role("button", name="Login").click()
-    page.wait_for_selector("css=canvas[height='265']", state='attached')
     assert page.url == "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
