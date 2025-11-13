ca3c97ef31c40b0a7f248fd481e5bce6049a6cfe
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-11-13 12:59:21 +0300

    Test elements

--- Tests-Playwright-Pytest-01/playwright-tests/test_elements.py
+++ Tests-Playwright-Pytest-01/playwright-tests/test_elements.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import Page, expect
+from playwright.sync_api import expect, Page
 
 def test_orange_hrm_title(page: Page):
     page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
@@ -6,6 +6,8 @@ def test_orange_hrm_title(page: Page):
     username.fill("Admin")
     password = page.get_by_placeholder("Password")
     password.fill("admin123")
+    expect(username).to_be_visible()
+    expect(password).to_be_editable()
     page.get_by_role("button", name="Login").click()
-    expect(page).to_have_url("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index")
-    expect(page).to_have_title("OrangeHRM")
+
+    
